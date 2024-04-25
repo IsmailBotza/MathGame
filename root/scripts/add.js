fae2fdc670ed28f63993692157494b49fdbdb86c
IsmailBotza <botza51@gmail.com>
2024-04-25 20:49:01 +0100

    ISMAIL Sala khdemto

--- root/scripts/add.js
+++ root/scripts/add.js
@@ -5,14 +5,28 @@ const audio =document.getElementById("MyAudio");
 var answer = 0;
 
 
-//OTMAN KAN HNA 
+ 
 function generate_equation(){
   var num1  = Math.floor(Math,random() * 13);
   var num2 = Math.floor(Math,random() * 13);
   var dumyAnswer1 = Math.floor(Math,random() * 13);
   var dumyAnswer2 = Math.floor(Math,random() * 13);
   var allAnswers = [];
-  var switchAnswers = [];  
+  var switchAnswers = []; 
+  
+  answer = num1 + num2;
+
+  document.getElementById("num1").innerHTML = num1 ;
+  document.getElementById("num2").innerHTML = num2 ;
+
+  allAnswers = [answer, dummyAnswer1, dummyAnswer2];
+  for(i = allAnswers.length;i--;){
+   switchAnswers.push(allAnswers.splice(Math.floor(Math.random()*(i+1)),1)[0]);
+  }
+
+  option1.innerHTML = answer;
+  option1.innerHTML = dummyAnswer1;
+  option1.innerHTML = dummyAnswer2;
   
  }
 
@@ -52,3 +66,5 @@ else{
 }
 
 });
+
+ 
